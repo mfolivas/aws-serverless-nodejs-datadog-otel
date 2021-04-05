fbcccfdfdd446b7ad8271f96824ad285a7203c77
Marcelo Olivas <mfolivas@gmail.com>
2021-04-05 12:10:43 -0400

    added the error logic and datadog-forwarder

--- handler.js
+++ handler.js
@@ -1,6 +1,12 @@
 'use strict';
 
 module.exports.hello = async (event) => {
+  const randomNumber = generateANumberFromOneToFifty()
+  if (randomNumber > 40) {
+    console.error('The number is greater than forty', randomNumber)
+    throw new Error('There was error with the lambda')
+  }
+  console.log('Random error is less than 40', randomNumber)
   return {
     statusCode: 200,
     body: JSON.stringify(
@@ -11,8 +17,15 @@ module.exports.hello = async (event) => {
       null,
       2
     ),
-  };
+  }
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+const generateNumber = (min, max) => {
+  return Math.floor(
+    Math.random() * (max - min + 1) + min
+  )
+}
+
+const generateANumberFromOneToFifty = () => {
+  return generateNumber(1, 50)
+}
